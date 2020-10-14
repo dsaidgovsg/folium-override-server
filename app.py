447f1840b17d13da06cc653ee9449b50b42318f3
Chen Weiguang <chen.weiguang@gmail.com>
2020-10-14 10:27:56 +0800

    feat(env): allow env vars to override app config
    Allow all "FLASK_APP_" prefixed env vars to be used to override the app
    config values dynamically.

--- app.py
+++ app.py
@@ -10,8 +10,24 @@ import os
 from dotenv import load_dotenv
 from flask import Flask
 from flask_compress import Compress
+import json
+from typing import Dict
 
 import fover.api.main as api_main
+
+
+def setup_app_conf(app: Flask, prefix: str) -> None:
+    envs = {k: v for k, v in os.environ.items() if k.startswith(prefix) and v}
+
+    for k, v in envs.items():
+        key = k[len(prefix) :]  # Strip the prefix
+
+        try:
+            app.config[key] = json.loads(v)
+        except ValueError as e:
+            # Value is a raw string
+            app.config[key] = v
+
 
 load_dotenv()
 
@@ -19,10 +35,10 @@ load_dotenv()
 #                   Defaults to the name of the static_folder folder.
 # static_folder – the folder with static files that should be served at static_url_path.
 #                 Defaults to the 'static' folder in the root path of the application.
+FLASK_APP_ENV_PREFIX = "FLASK_APP_"
 app = Flask(__name__)
-app.config["COMPRESS_LEVEL"] = 9
-app.config["COMPRESS_ALGORITHM"] = ["gzip"]
 
+setup_app_conf(app, FLASK_APP_ENV_PREFIX)
 Compress(app)
 
 app.register_blueprint(api_main.bp)
